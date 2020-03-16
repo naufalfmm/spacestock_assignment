3abfd1209389ea12c4fc984b24f7313db19dab68
Muhammad Naufal Farras Mastika <naufal@nodeflux.io>
2020-03-16 11:11:08 +0700

    change db

--- Assignment_7/db/learn_golang_20200316_110958.sql
+++ Assignment_7/db/learn_golang_20200316_110958.sql
@@ -16,6 +16,28 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: learn_golang; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE learn_golang WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+ALTER DATABASE learn_golang OWNER TO postgres;
+
+\connect learn_golang
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 --
